e24a9790c8a42865a970a983ba7825878c37766c
Ferhat <ferhat.yousfi@gmail.com>
2022-03-28 22:27:00 +0200

    Initialise le projet avec Spring Initialzr

---
+++ src/main/java/fr/ferhat/mailreader/MailreaderApplication.java
@@ -0,0 +1,13 @@
+package fr.ferhat.mailreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ferhat/mailreader/MailreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ferhat.mailreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
